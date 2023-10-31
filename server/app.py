3dd3f13ba12aaba56de643d8042129e01a18dc12
keyla <keyyleiva@gmail.com>
2023-10-30 22:44:00 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -25,6 +25,75 @@ db.init_app(app)
 def home():
     return ''
 
+@app.route('/scientists', methods = ['GET', 'POST'])
+def scientists():
+    if request.method == 'GET':
+        scientists = Scientist.query.all()
+        scientists_dict = [scientist.to_dict() for scientist in scientists]
+
+        return make_response(scientists_dict, 200)
+    
+    elif request.method == 'POST':
+        form_data = request.get_json()
+
+        try:
+
+            new_scientist = Scientist(
+                name = form_data['name'],
+                field_of_study = form_data['field_of_study']
+            )
+
+            db.session.add(new_scientist)
+
+            db.session.commit()
+            
+            new_scientist_dict = new_scientist.to_dict()
+
+            return make_response(new_scientist_dict, 201)
+        
+        except KeyError:
+
+            return make_response({ "validation errors" : None }, 400)
+
+    
+@app.route('/scientists/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
+def scientist_by_id(id):
+    
+    scientist = Scientist.query.filter_by(id = id).first()
+
+    if scientist:
+        if request.method == 'GET':
+            scientist_dict = scientist.to_dict()
+            return make_response(scientist_dict, 200)
+        
+        elif request.method == 'PATCH':
+            form_data = request.get_json()
+
+            try:
+                for attr in form_data:
+                    setattr(scientist, attr, form_data.get(attr))
+                
+                db.session.commit()
+
+                return make_response(scientist.to_dict(), 201)
+            
+            except KeyError:
+
+                return make_response({ "validation errors" : None }, 403)
+            
+        elif request.method == 'DELETE':
+
+            db.session.delete(scientist)
+
+            db.session.commit()
+
+            return make_response({}, 202)
+
+    else: 
+        return make_response("Scientist not found", 404)
+
+    
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
